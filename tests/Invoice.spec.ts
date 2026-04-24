4355ecb6cb9c1713dc9cb4a496c7a65590d0661c
Arisa <arisa@welovebug.biz>
2026-04-24 10:35:42 +0700

    [Added]: เพิ่ม Test Scenarios Name: Tax Invoice with two item - add tree item but delete one item then submit successfully ในไฟล์ Invoice.spec.ts

--- tests/Invoice.spec.ts
+++ tests/Invoice.spec.ts
@@ -39,9 +39,78 @@ test('Tax Invoice with one item - edit price and qty then submit successfully', 
     await page.getByTestId('submit-button').click()
     // ตรวจสอบ url Invoice Succsee
   await expect(page).toHaveURL('https://ui-sandbox-omega.vercel.app/invoice/success');
-
     // 7. ตรวจสอบข้อความบนหน้าเว็บ 
   await expect(page.getByTestId('success-heading')).toHaveText('Submit Invoice Successful')
+   })
+
+   test('Tax Invoice with two item - add tree item but delete one item then submit successfully', async ({page}) => {
+    // 1. Go-to- Invoice-Pacel
+    await page.goto('https://ui-sandbox-omega.vercel.app/invoice')
+    // 2. Select Document type
+    const documentTypeDropfown = page.getByTestId('invoice-type-select')
+    await documentTypeDropfown.selectOption({label: 'Tax Invoice'})
+    // 3. Fill First•Item-Name
+    await page.getByTestId('row-1-name').fill('C-Level Consulting')
+    // 4. Search-for-Consulting in category
+    // Select-Consulting-in category field
+    await page.getByTestId('row-1-category').fill('Co')
+    await page.getByRole('option', { name: 'Consulting' }).click()
+    // 5. Fill First Item Unit-Price
+    await page.getByTestId('row-1-unit-price').fill('2000')
+    // 6. clear Fill and fill First Item-Qty 
+    await page.getByTestId('row-1-qty').clear()
+    await page.getByTestId('row-1-qty').fill('4')
+    await page.locator('body').click()
+    // 7. Verify First Item Total Price to be 8,000.00
+    await expect(page.getByTestId('row-1-total')).toHaveValue('8,000.00')
+    // 8. Add Secound Item 
+    await page.getByTestId('add-row-button').click()
+    // 9. Fill Secound Item-Name
+    await page.getByTestId('row-2-name').fill('E2E Automation Test via APIs')
+    // 10. Search-for-Maintenance in category
+    // 11. Select-Maintenance in category field
+    await page.getByTestId('row-2-category').fill('Mai')
+    await page.getByRole('option', { name: 'Maintenance' }).click()
+    // 12. Fill Secound Item Unit-Price
+    await page.getByTestId('row-2-unit-price').fill('10,000.00')
+    // 13. clear Fill and fill secound Item-Qty 
+    await page.getByTestId('row-2-qty').clear()
+    await page.getByTestId('row-2-qty').fill('1')
+    await page.locator('body').click()
+    // 14. Verify Secound Item Total Price to be 10,000.00
+    await expect(page.getByTestId('row-2-total')).toHaveValue('10,000.00')
+    // 15. Add Thrid Item 
+    await page.getByTestId('add-row-button').click()
+    // 16. Fill Third Item-Name
+    await page.getByTestId('row-3-name').fill('E2E Automation Test via APIs')
+    // 17. Search-for-Design in category
+    // Select-Design in category field
+    await page.getByTestId('row-3-category').fill('Des')
+    await page.getByRole('option', { name: 'Design' }).click()
+    // 18. Fill Third Item Unit-Price
+    await page.getByTestId('row-3-unit-price').fill('1,000.00')
+    // 19. clear Fill and fill third Item-Qty 
+    await page.getByTestId('row-3-qty').clear()
+    await page.getByTestId('row-3-qty').fill('2')
+    await page.locator('body').click()
+    // 20. Verify third Item Total Price to be 2,000.00
+    await expect(page.getByTestId('row-3-total')).toHaveValue('2,000.00')
+    // 21  Verify Subtotal to be 20,000.00
+    await expect(page.getByTestId('summary-subtotal')).toHaveText('20,000.00')
+    // 22. Verify VAT (7%)  to be 1,400.00
+    await expect(page.getByTestId('summary-vat')).toHaveText('1,400.00')
+    // 23. Verify Grand total Price to be 21,400.00
+    await expect(page.getByTestId('summary-grand-total')).toHaveText('21,400.00')
+    // 24. Delete third Item 
+    await page.getByTestId('row-3-delete-checkbox').click()
+    await page.getByTestId('delete-selected-button').click()
+    // 25  Verify Subtotal to be 18,000.00
+    await expect(page.getByTestId('summary-subtotal')).toHaveText('18,000.00')
+    // 26. Verify VAT (7%)  to be 1,260.00
+    await expect(page.getByTestId('summary-vat')).toHaveText('1,260.00')
+    // 27. Verify Grand total Price to be 19,260.00
+    await expect(page.getByTestId('summary-grand-total')).toHaveText('19,260.00')
+    
    })
    
 
